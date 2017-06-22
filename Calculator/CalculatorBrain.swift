197dff6ae5b3c09a200791226934ba6bfa7c7a7c
Jonny Mac <jonnymac@Mac-mini.local>
2017-06-22 07:00:05 -0700

    calculator model

---
+++ Calculator/CalculatorBrain.swift
@@ -0,0 +1,24 @@
+//
+//  CalculatorBrain.swift
+//  Calculator
+//
+//  Created by Jonny Mac on 6/21/17.
+//  Copyright © 2017 Jonny Mac. All rights reserved.
+//
+
+import Foundation
+
+struct CalculatorBrain {
+    
+    func performOperation(_ symbol: String) {
+        
+    }
+    
+    func setOperand(_ operand: Double) {
+        
+    }
+    
+    var result: Double {
+        get {  }
+    }
+}
